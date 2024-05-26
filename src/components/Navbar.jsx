ebb4bf19ba5d6d24e1dde7eb97a6d0fbd0cf55bc
Diana2x <dianaclxcoe@gmail.com>
2024-05-27 01:59:23 +0200

    added tailwind and other dependencies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
   )
 }
 
