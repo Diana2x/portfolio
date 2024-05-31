150e20a81768b8e2d5118d6342f9b48828ba5502
Diana2x <dianaclxcoe@gmail.com>
2024-06-01 00:46:28 +0200

    modified details and added About me section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {TypeAnimation} from 'react-type-animation'
 import lgbackground from '../assets/lg-bg.png'
 import dianaPicture from '../assets/diana.png'
-import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import { FaFacebook, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa'
 
 function Main() {
   return (
@@ -14,9 +14,9 @@ function Main() {
             <h1 className='flex sm:text-5xl text-4xl pt-4 font-bold text-gray-800'>I'm  
               <TypeAnimation
                 sequence={[
-                  'Diana Cordero',
+                  ' Diana Cordero',
                   2000,
-                  'a Web Developer',
+                  ' a Web Developer',
                   2000,
                 ]}
                 wrapper="span"
@@ -26,13 +26,13 @@ function Main() {
               />
             </h1>
             <div>
-              <p className='py-6'>I'm passionate about technology, with experience in customer service and logistics. I currently work as a Business Systems Analyst, where I use my skills to improve business processes. My passion for technology drives me to seek innovative solutions in the IT sector. I am excited to be a valuable asset to any team.</p>
+              <p className='py-6 text-xl text-justify'>I'm passionate about technology and learning programming, with experience in customer service and logistics. I currently work as a Business Systems Analyst, where I use my skills to improve business processes. My enthusiasm for web systems development drives me to seek innovative solutions in the IT sector. I am excited to deepen my knowledge and be a valuable asset to any team.</p>
             </div>
             <div className='flex justify-between pt-6 max-w-[200px] w-full'>
-              <FaTwitter className='cursor-pointer' size={20} />
-              <FaFacebook className='cursor-pointer' size={20} />
-              <FaInstagram className='cursor-pointer' size={20} />
+              <FaGithub className='cursor-pointer' size={20} />
               <FaLinkedin className='cursor-pointer' size={20} />
+              <FaInstagram className='cursor-pointer' size={20} />
+              <FaFacebook className='cursor-pointer' size={20} />
             </div>
           </div>
           <div className='lg:ml-8 mt-8 lg:mt-0'>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,9 +7,8 @@ const Projects = () => {
   return (
     <div id="projects" className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
       <h1 className='text-4xl font-bold text-center text-gray-800'>Projects</h1>
-      <p className='text-justify py-8 text-gray-600'>
-        Here are some of the projects I have worked on. Click on the images to view the project.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium corporis voluptatum laudantium aliquid maxime atque deserunt soluta, odit eius fugit provident quis iste dolorem nihil voluptatibus. Alias nisi nemo molestias.
+      <p className='text-justify flex justify-center py-8 text-gray-600'>
+      Here are some of the projects I have worked on. Hover over the images to discover more details about each one.
       </p>
       <div className='grid sm:grid-cols-2 gap-12'>
         <ProjectItem img={weatherImg} title="Weather App" language="React JS"/>
